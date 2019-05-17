5145a976ccbf38b63b6b40259497b18983590002
mstechly <michal.stechly@gmail.com>
2019-05-17 19:03:09 +0200

    Added try-catch in api.js

--- src/services/api.js
+++ src/services/api.js
@@ -1,6 +1,7 @@
 import superagent from 'superagent'
 
 export async function solve(distances) {
+    try {
     const {body} = await superagent.post('/tsp/solve')
         .send({
             distances,
@@ -11,4 +12,18 @@ export async function solve(distances) {
         .accept('application/json');
 
     return body;
+    }
+    catch (e) {
+        const {body} = await superagent.post('/tsp/solve')
+            .send({
+                distances,
+                start_node: 0,
+                end_node: distances.length - 1,
+                use_dwave: false
+            })
+            .accept('application/json');
+
+    return body;
+
+    }
 }
