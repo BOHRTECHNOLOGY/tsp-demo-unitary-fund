27b80046df77197a7a27721af5c8563275492c56
Janusz Grzesik <janusz@estimote.com>
2019-10-17 17:00:26 +0200

    Add possibility to inject api key via REACT_APP_GOOGLE_MAPS_API_KEY.

--- src/config/config.js
+++ src/config/config.js
@@ -1,4 +1,4 @@
-export const GOOGLE_MAPS_API_KEY = 'AIzaSyDsxzbYc_IIAeqHXkxDWJd5H2DORBd9uY0';
+export const GOOGLE_MAPS_API_KEY = process.env.REACT_APP_GOOGLE_MAPS_API_KEY || '';
 
 export const MAX_SELECTED_POINTS_COUNT = 9;
 
